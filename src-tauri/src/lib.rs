79390965d2f22b451c16a57c3d6b25ae3afbd81c
Haoming <hmstudy02@gmail.com>
2025-05-12 13:54:25 +0800

    init
    npm create tauri-app@latest

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,12 @@
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
