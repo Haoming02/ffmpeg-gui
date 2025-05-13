8dd64e02be7579d5989ed181f514867dfbb0d258
Haoming <hmstudy02@gmail.com>
2025-05-13 11:28:21 +0800

    paths

--- src/main.js
+++ src/main.js
@@ -1,28 +1,42 @@
 const { invoke } = window.__TAURI__.core;
 const { open: openDialog } = window.__TAURI__.dialog;
 
+/** @type {HTMLInputElement} */ let inputPath;
+/** @type {HTMLInputElement} */ let outputPath;
+/** @type {HTMLImageElement} */ let settings;
+
+/** @type {boolean} */ let batch = true;
+
 async function preload() {
   /** @type {boolean} */
   const hasFFmpeg = await invoke("has_ffmpeg");
   if (hasFFmpeg) return;
 
-  const img = document.getElementById("title");
-  img.querySelector("img").removeAttribute("hidden");
+  const warning = document.getElementById("warning");
+  warning.removeAttribute("hidden");
+  warning.onclick = () => window.open("https://ffmpeg.org/download.html", "_blank");
+}
+
+function init() {
+  inputPath = document.getElementById("input");
+  outputPath = document.getElementById("output");
+  settings = document.getElementById("settings");
 }
 
 window.addEventListener("DOMContentLoaded", () => {
   preload();
+  init();
 
-  const folderInput = document.getElementById("input");
-
-  folderInput.addEventListener("dblclick", async () => {
-    const selected = await openDialog({
-      directory: true,
-      multiple: false,
-      defaultPath: ".",
-    });
-
-    if (selected) folderInput.value = selected;
+  inputPath.addEventListener("dblclick", async () => {
+    const path = await openDialog({ directory: batch, multiple: false, defaultPath: "." });
+    if (path) {
+      inputPath.value = path;
+      outputPath.value = path;
+    }
   });
 
+  outputPath.addEventListener("dblclick", async () => {
+    const path = await openDialog({ directory: batch, multiple: false, defaultPath: "." });
+    if (path) outputPath.value = path;
+  });
 });
