d83732ec60bb4d7637b12a28988b5922ea918e4a
Haoming <hmstudy02@gmail.com>
2025-05-15 10:58:29 +0800

    calculate progress

--- src-tauri/src/func.rs
+++ src-tauri/src/func.rs
@@ -1,6 +1,27 @@
-use std::io::Error;
+use std::io::{BufRead, BufReader, Error};
 use std::path::Path;
-use std::process::{Command, ExitStatus};
+use std::process::{Command, ExitStatus, Stdio};
+
+fn parse_seconds(timestamp: &str) -> f32 {
+    let mut time: f32 = 0.0;
+
+    let times: Vec<&str> = timestamp.split(':').collect();
+    if times.len() != 3 {
+        return time;
+    }
+
+    if let Ok(hour) = times[0].parse::<f32>() {
+        time += hour * 60.0 * 60.0;
+    }
+    if let Ok(min) = times[1].parse::<f32>() {
+        time += min * 60.0;
+    }
+    if let Ok(sec) = times[2].parse::<f32>() {
+        time += sec;
+    }
+
+    time
+}
 
 fn run_single(input: &String, output: &String, args: &Vec<String>) -> Result<ExitStatus, Error> {
     let mut command = Command::new("ffmpeg");
@@ -12,7 +33,37 @@ fn run_single(input: &String, output: &String, args: &Vec<String>) -> Result<Exi
     }
     command.arg(&output);
 
-    let status = command.status()?;
+    command.arg("-progress").arg("pipe:2");
+    command.stderr(Stdio::piped());
+
+    let mut child = command.spawn()?;
+    let stderr = child.stderr.take().unwrap();
+    let reader = BufReader::new(stderr);
+
+    let mut total: f32 = -1.0;
+
+    for line_result in reader.lines() {
+        if let Ok(line) = line_result {
+            if total < 0.0 && line.trim().starts_with("Duration:") {
+                let temp = line
+                    .split("Duration:")
+                    .last()
+                    .unwrap()
+                    .split(", start")
+                    .next()
+                    .unwrap()
+                    .trim();
+                total = parse_seconds(&temp);
+                println!("Duration: {}", total);
+            } else if line.contains("out_time=") {
+                let temp = line.split("out_time=").last().unwrap().trim();
+                let current = parse_seconds(&temp);
+                println!("Progress: {}%", current * 100.0 / total);
+            }
+        }
+    }
+
+    let status = child.wait()?;
     Ok(status)
 }
 
